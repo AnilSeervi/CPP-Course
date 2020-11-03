e26692de53d3be1dc6162b0c57642991ab81cc50
Anil Seervi <anil13112000@gmail.com>
2020-11-03 17:36:37 +0530

    Patterns-1.

---
+++ 8. Awesome Pattern Questions-1/pattern7.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j<=i){
+                cout<<"*";
+                }else{
+                    cout<<" ";
+                }
+            }
+                for(int k=1;k<=n;k++){
+                    if(k<=n-i){
+                        cout<<" ";
+                    }else{
+                        cout<<"*";
+                    }
+                }
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(j<=i){
+                cout<<"*";
+                }else{
+                    cout<<" ";
+                }
+            }
+                for(int k=1;k<=n;k++){
+                    if(k<=n-i){
+                        cout<<" ";
+                    }else{
+                        cout<<"*";
+                    }
+                }
+        cout<<endl;
+    }
+    return 0;
+}
